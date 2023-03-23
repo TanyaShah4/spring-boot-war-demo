e6963cc711386969b90e8ce8030cce0a8d3985ea
Tanya Shah <90772341+TanyaShah4@users.noreply.github.com>
2023-03-23 19:02:23 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "please visit my medium blog on jenkins";
+        return "Hey there !";
     }
 
 }
